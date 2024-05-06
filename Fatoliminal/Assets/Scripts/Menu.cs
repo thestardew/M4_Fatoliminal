f6ef340bcebbe51f3577175f05514e7043dad686
thestardew <ka4an2283@gmail.com>
2024-05-06 15:47:18 +0300

    first commit
    Проверяем

---
+++ Fatoliminal/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Fatoliminal/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float value = 100;
+    private float _maxValue;
+    public GameObject gameplayUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _maxValue = value;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+ 
+    }
+
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if (value <= 0)
+        {
+            PlayerIsDead();
+        }
+    }
+
+    private void PlayerIsDead()
+    {
+        gameplayUI.SetActive(false);
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<CameraRotation>().enabled = false;
+    }  
+}
+
+
+
